0f00180d2b1299248d538f5ae6342bf4e773f838
SURAJ KUMAR MAHATO <suraj.mahato.4166@gmail.com>
2023-02-28 23:28:30 +0530

    Addition of Two Numbers

---
+++ Addition of Two Numbers.java
@@ -0,0 +1,12 @@
+/*
+Addition of Two Numbers
+Given two numbers A and B. Your task is to return the sum of A and B.
+*/
+class Solution
+{
+    static int addition(int A, int B)
+{
+        // code here
+        return A+B;
+    }
+}
