725c058de95aa6ad9c64d18c9213f6f7585208c8
SURAJ KUMAR MAHATO <suraj.mahato.4166@gmail.com>
2023-02-28 23:22:23 +0530

    C++ Input / Output

---
+++ C++ Input Output.java
@@ -0,0 +1,12 @@
+/*
+C++ Input / Output
+Given two integers A and B. Your task is to return their product.
+*/
+class Solution
+{
+    static int multiplication(int A, int B)
+    {
+        // code here
+        return A*B;
+    }
+}
