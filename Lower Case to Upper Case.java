c904ca4afcdb61dc092cc559881f0087a26c8a7e
SURAJ KUMAR MAHATO <suraj.mahato.4166@gmail.com>
2023-03-02 23:56:13 +0530

    Lower Case to Upper Case Added

---
+++ Lower Case to Upper Case.java
@@ -0,0 +1,14 @@
+/*
+Lower Case to Upper Case
+Given a string str containing only lowercase letters, 
+generate a string with the same letters, but in uppercase.
+*/
+
+class Solution 
+{ 
+    String to_upper(String str) 
+    { 
+        // code here
+        return str.toUpperCase();
+    }
+}
