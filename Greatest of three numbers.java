2a9cf19b583dee5c41405d7897f7d0fa384f296f
SURAJ KUMAR MAHATO <suraj.mahato.4166@gmail.com>
2023-03-05 23:14:04 +0530

    Greatest of three numbers Added

---
+++ Greatest of three numbers.java
@@ -0,0 +1,17 @@
+/*
+Greatest of three numbers
+*/
+
+class Solution {
+  public:
+    int greatestOfThree(int A, int B, int C) 
+    {
+        // code here
+        if(A>B && A>C)
+            return A;
+        else if(B>A && B>C)
+            return B;
+        else
+            return C;
+    }
+};
