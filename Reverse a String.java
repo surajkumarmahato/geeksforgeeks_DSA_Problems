96f7f6c8b9d05c9c6f46ae55fc113529467bd83c
SURAJ KUMAR MAHATO <suraj.mahato.4166@gmail.com>
2023-03-06 23:17:23 +0530

    Reverse a String

---
+++ Reverse a String.java
@@ -0,0 +1,20 @@
+/*
+Reverse a String
+*/
+
+class Solution 
+{
+    static String revStr(String S) 
+    {
+        // code here
+        char ch;
+        String nstr="";
+        for (int i=0; i<S.length(); i++)
+        {
+            ch= S.charAt(i);
+            nstr= ch+nstr;
+        }
+        return nstr;
+        
+    }
+}
