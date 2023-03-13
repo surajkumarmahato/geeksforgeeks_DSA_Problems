596c04cc77e34ad5d00ec9adea14501d14c25217
SURAJ KUMAR MAHATO <suraj.mahato.4166@gmail.com>
2023-03-13 23:07:33 +0530

    C++ Strings File Added

---
+++ C++ Strings.cpp
@@ -0,0 +1,9 @@
+/*
+C++ Strings
+Given two strings  S1 and S2 . You have to concatenate both the strings and print the concatenated string.
+*/
+string conCat(string s1 , string s2)
+{
+    // code here.
+    return s1+s2;
+}
